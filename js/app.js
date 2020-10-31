80cf982bb27a0565615423e2c1f504b244ee8bec
ilhomjon55 <ilhomshamidinov55@gmail.com>
2020-10-31 14:06:28 +0500

    Add advanced time functions to js and make svgs animated

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 var elMainForm = document.querySelector('.main-form');
 var elPiyodaResult = document.querySelector('.piyoda-item');
 var elVelikResult = document.querySelector('.velik-item');
-var elMoshinResult = document.querySelector('.moshin-item');
-var elSamalyotResult = document.querySelector('.samalyot-item');
+var elCarResult = document.querySelector('.moshin-item');
+var elPlaneResult = document.querySelector('.samalyot-item');
 
 // STANDARD DISTANCES
 var piyodaSpeed = 3.6;
 var velikSpeed = 20.1;
-var moshinSpeed = 70;
-var samalyotSpeed = 800;
+var carSpeed = 70;
+var planeSpeed = 800;
 
 elMainForm.addEventListener('submit', function (evt) {
 
@@ -16,16 +16,34 @@ elMainForm.addEventListener('submit', function (evt) {
 
   var targetDistance = parseFloat(elMainForm.querySelector('.distance-input').value.trim(), 10);
   targetDistance.toFixed(2);
-  var targetResultPiyoda = (targetDistance / piyodaSpeed).toFixed(2);
-  var targetResultVelik = (targetDistance / velikSpeed).toFixed(2);
-  var targetResultMoshin = (targetDistance / moshinSpeed).toFixed(2);
-  var targetResultSamalyot = (targetDistance / samalyotSpeed).toFixed(2);
+
+  // Piyoda calc
+  var targetResultPiyodaHour = Math.floor(targetDistance / piyodaSpeed);
+  var targetResultPiyodaMinute = Math.round(((targetDistance / piyodaSpeed) - targetResultPiyodaHour) * 60);
+  var targetResultPiyoda = `${targetResultPiyodaHour} soat, ${targetResultPiyodaMinute} minut`
+
+  // Velik calc
+  var targetResultVelikHour = Math.floor(targetDistance / velikSpeed);
+  var targetResultVelikMinute = Math.round(((targetDistance / velikSpeed) - targetResultVelikHour) * 60);
+  var targetResultVelik = `${targetResultVelikHour} soat, ${targetResultVelikMinute} minut`
+
+  // Car calc
+  var targetResultCarHour = Math.floor(targetDistance / carSpeed);
+  var targetResultCarMinute = Math.round(((targetDistance / carSpeed) - targetResultCarHour) * 60);
+  var targetResultCar = `${targetResultCarHour} soat, ${targetResultCarMinute} minut`
+
+  // Plane calc
+  var targetResultPlaneHour = Math.floor(targetDistance / planeSpeed);
+  var targetResultPlaneMinute = Math.round(((targetDistance / planeSpeed) - targetResultPlaneHour) * 60);
+  var targetResultPlane = `${targetResultPlaneHour} soat, ${targetResultPlaneMinute} minut`
+
+
 
   if (targetDistance > 0) {
     elPiyodaResult.textContent = targetResultPiyoda;
     elVelikResult.textContent = targetResultVelik;
-    elMoshinResult.textContent = targetResultMoshin;
-    elSamalyotResult.textContent = targetResultSamalyot;
+    elCarResult.textContent = targetResultCar;
+    elPlaneResult.textContent = targetResultPlane;
   } else if (isNaN(targetDistance)) {
     elPiyodaResult.textContent = '0';
     elVelikResult.textContent = '0';
@@ -38,7 +56,7 @@ elMainForm.addEventListener('submit', function (evt) {
     elVelikResult.textContent = '0';
     elMoshinResult.textContent = '0';
     elSamalyotResult.textContent = '0';
-    alert(`Musbat raqam kiritng birodar!`);
+    alert(`Musbat raqam kiriting birodar!`);
   }
 
 });
